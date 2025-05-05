24621a545fcdfb41391232cb1f2296dabb7b9a87
Anatoliy Shikin <t7313@ya.ru>
2025-05-05 15:51:16 +0200

    done

--- src/main/resources/db/changelog/add_author_and_book_proc.sql
+++ src/main/resources/db/changelog/add_author_and_book_proc.sql
@@ -1,12 +1,16 @@
 --liquibase formatted sql
---changeset ashikin:add_author_and_book_proc
+
+--changeset ashikin:add_author_and_book_proc splitStatements:false endDelimiter:$$;
 CREATE OR REPLACE PROCEDURE add_author_and_book_proc(
     in_p_first_name   VARCHAR,
     in_p_last_name    VARCHAR,
     in_p_title        VARCHAR,
     in_p_published    INTEGER,
     in_p_total_copies INTEGER
-) AS
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
   new_author_id INTEGER;
 BEGIN
   INSERT INTO authors(first_name, last_name)
@@ -14,5 +18,6 @@ BEGIN
   RETURNING id INTO new_author_id;
   INSERT INTO books(title, author_id, published_year, total_copies)
   VALUES (in_p_title, new_author_id, in_p_published, in_p_total_copies);
-END add_author_and_book_proc;
+END;
+$$;
 --rollback DROP PROCEDURE add_author_and_book_proc;
