c2954cfbf4ead78612bd476fefaff109641f0b5f
Anatoliy Shikin <t7313@ya.ru>
2025-05-21 19:12:35 +0200

    тест

--- src/main/resources/db/changelog/create-authors.sql
+++ src/main/resources/db/changelog/create-authors.sql
@@ -3,7 +3,7 @@
 --changeset ashikin:create-authors
 CREATE TABLE authors
 (
-  id         SERIAL PRIMARY KEY,
+  id         UUID PRIMARY KEY,
   first_name VARCHAR(50) NOT NULL,
   last_name  VARCHAR(50) NOT NULL
 );
